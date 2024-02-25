b8cffea5fa6f2bd820228e942bd2e30cbd164d2f
PaskeyDuet <158359587+PaskeyDuet@users.noreply.github.com>
2024-02-25 21:54:46 +0300

    250224

--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,13 @@
 const table = [];
-for(let i = 0; i < 13; i++){
-     table.push([]);
-     for(let j = 0; j < 10; j++){
-          table[i].push(j);
-     }
-}
-
 const field = document.querySelectorAll('.Field_zone_playground');
 
 var tableHtml = '<table class=field>';
-for (var i = 0; i < 13; i++) {
-     tableHtml += '<tr class="row">';
-    for (var j = 0; j < 10; j++) {
-        tableHtml += '<td class="cell">' + '<div class="field_img_container"></div>' + table[i][j] + '</td>';
+for (let i = 0; i < 8; i++) {
+          table.push([]);
+          tableHtml += '<tr class="row">';
+    for (let j = 0; j < 18; j++) {
+          table[i].push(`${i}.${j}`);
+          tableHtml += '<td class="cell">' + '<div class="field_img_container"></div>' + table[i][j] + '</td>';
     }
     tableHtml += '</tr>';
 }
