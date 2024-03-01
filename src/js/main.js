1e5008edbaa90cb82a1688c8872b0c7ba0f6a3d6
PaskeyDuet <158359587+PaskeyDuet@users.noreply.github.com>
2024-03-01 12:22:07 +0300

    BEM classes updates

--- src/js/main.js
+++ src/js/main.js
@@ -4,10 +4,10 @@ const field = document.querySelectorAll('.field-zone__playground');
 let tableHtml = '<table class=field-zone__playground>';
 for (let i = 0; i < 8; i++) {
           table.push([]);
-          tableHtml += '<tr class="field-zone__playground__row">';
+          tableHtml += '<tr class="playground__row">';
     for (let j = 0; j < 18; j++) {
           table[i].push(`${i}.${j}`);
-          tableHtml += '<td class="field-zone__playground__cell">' + '<div class="field__img__container"></div>' + table[i][j] + '</td>';
+          tableHtml += '<td class="playground__cell">' + '<div class="field__img__container"></div>' + table[i][j] + '</td>';
     }
     tableHtml += '</tr>';
 }
@@ -17,9 +17,13 @@ field.forEach(function(field) {
      field.innerHTML = tableHtml;
  });
 
+//  start td.playground__cell = based
+// mined + .playground__cell_wood = td.playground__cell.playground__cell_wood
+// increment counter wood 
+
  
-let crystalImg = document.querySelectorAll('.field-zone__playground__cell')
-let crystalText = document.querySelector('.bottom-menu__counter__rock-block__text')
+let crystalImg = document.querySelectorAll('.playground__cell')
+let crystalText = document.querySelector('.resource__mined_wood')
 
 let crystalNumber = 0;
 crystalText.innerHTML = crystalNumber;
